392e9157f4a76faa8b59f5cb147344cbee61737a
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-03-27 11:35:46 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CalculatorLogo from "./assets/calculator-logo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="header">
+      <img src={CalculatorLogo} alt="Calculator Logo" />
+      <h1>Calculator</h1>
+    </div>
+  );
+}
+
+export default App;
